a0662305fab05d428e03ef4127db12ff26c66e9c
Luigi Barte <luigibarte1@gmail.com>
2025-11-05 21:04:19 +0800

    Add files via upload

---
+++ reverseStrint.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		String name = "luigi";
+		String reverse = "";
+		
+		for(int i = name.length() - 1; i >= 0; i--){
+			reverse = reverse + name.charAt(i);
+		}
+		
+		System.out.println(reverse);
+	}
+}
