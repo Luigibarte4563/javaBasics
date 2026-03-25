474e774ee51db5f002f23128db9011934d82e63b
Luigi Barte <luigibarte1@gmail.com>
2026-03-25 19:58:39 +0800

    Add files via upload

---
+++ enterName.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Enter your name: ");
+		String name = s.nextLine();
+		
+		System.out.println();
+		System.out.print("your name is "  + name);
+	}
+}
